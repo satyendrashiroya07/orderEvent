3996a2743390820d831c86fa0231a8e31c03ecdc
satyendrashiroya07 <shiroya0749@gmail.com>
2026-04-14 23:03:11 +0530

    working for security level allowing only user comes from API gateWay no direct access

---
+++ src/main/java/productEvent/userEvent/UserDtoFeing.java
@@ -0,0 +1,10 @@
+package productEvent.userEvent;
+
+import lombok.Data;
+
+@Data
+public class UserDtoFeing {
+        private String userId;
+        private String userName;
+        private String userEmail;
+}
